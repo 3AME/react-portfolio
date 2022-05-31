37e4c3126d5ab123eb0780785edd7dd9fc93eda1
3AME <45684631+3AME@users.noreply.github.com>
2022-05-31 20:44:10 +0300

    portfolio-Modify testimonials and contact information

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,18 +14,18 @@ function Footer() {
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
+        {/* <li><a href="#testimonials">Testimonials</a></li> */}
         <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className="footer__socials">
         <a href="https://facebook.com"><FaFacebookF /></a>
         <a href="https://instagram.com"><FiInstagram /></a>
-        <a href="https://twitter.com"><IoLogoTwitter /></a>
+        {/* <a href="https://twitter.com"><IoLogoTwitter /></a> */}
       </div>
 
       <div className="footer__copyright">
-        <small>&copy; ANFANG Tutorials. All rights reserved.</small>
+        <small>&copy; ANFANG. All rights reserved.</small>
       </div>
     </footer>
   )
