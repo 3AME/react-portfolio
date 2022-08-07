ad0cbe71b45de03fe5232ec5470eaa15179c2c8d
3AME <45684631+3AME@users.noreply.github.com>
2022-08-07 21:59:42 +0300

    update cv and change the header button style to material ui button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CV from '../../assets/CV.pdf'
+import Button from '@mui/material/Button';
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href='#contact' className='btn btn-primary'>Let's talk</a>
+      <Button variant='contained' ><a href={CV} download>Download CV</a></Button>
+        {/* <a href={CV} download className='btn'>Download CV</a> */}
+        {/* <a href='#contact' className='btn btn-primary'>Let's talk</a> */}
+        <Button variant='contained' > <a href='#contact' >Let's talk</a></Button>
     </div>
   )
 }
