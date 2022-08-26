7eeeaf4355b13b3baec43ebeea93ffc8b69dc37d
3AME <45684631+3AME@users.noreply.github.com>
2022-08-26 17:31:24 +0300

    Update two projects: Chinese recipes, daily life in tampere

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,6 +6,8 @@ import IMG3 from "../../assets/portfolio3.png";
 import IMG4 from "../../assets/portfolio4.png";
 import IMG5 from "../../assets/portfolio5.png";
 import IMG6 from "../../assets/portfolio6.png";
+import IMG7 from "../../assets/dailyLifeInTampere.png";
+import IMG8 from "../../assets/ChineseRecipes.png"
 
 const data = [
   
@@ -17,14 +19,14 @@ const data = [
     demo: "https://www.figma.com/proto/hnIBtfsObyv49cMTpb4zAX/Prototype-YLE?page-id=0%3A1&node-id=2%3A5&viewport=186%2C347%2C0.28&scaling=scale-down&starting-point-node-id=2%3A5",
   },
   {
-    id: 1,
+    id: 2,
     image: IMG4,
     title: "Activities around you",
     github: "#",
     demo: "https://www.figma.com/proto/nvxof2P90dbVfs80clRs8N/hti110-group-work?page-id=103%3A233&node-id=205%3A2762&viewport=198%2C160%2C0.22&scaling=scale-down&starting-point-node-id=205%3A2762",
   },
   {
-    id: 1,
+    id: 3,
     image: IMG3,
     title: "Design of everyday behavior",
     github: "#",
@@ -34,30 +36,46 @@ const data = [
 
 const data1 = [
   {
-    id: 1,
+    id: "id1",
     image: IMG5,
     title: "Algorithm-based search for optimal delivery routes",
     github: "https://github.com/3AME/new_Delivery",
     demo: "#",
   },
   {
-    id: 1,
+    id: "id2",
     image: IMG6,
     title: "New epidemic data analysis",
     github: "https://github.https://github.com/3AME/new-epidemic-data-analysis",
     demo: "#",
   },
+  
 
 ]
 const data2 = [
   {
-    id: 1,
+    id: "gd1",
+    image: IMG7,
+    title: "Daily life in Tampere",
+    github: "https://github.com/3AME/daily-life-in-tampere",
+    demo: "https://anfang-daily-life-in-tampere.vercel.app/",
+  },
+  {
+    id: "gd2",
+    image: IMG8,
+    title: "Chinese recipes for a friend",
+    github: "https://github.com/3AME/lbs-birthday-recipes",
+    demo: "https://lbs-birthday-recipes.vercel.app/",
+  },
+  {
+    id: "gd3",
     image: IMG1,
     title:
       "Portfolio created by React",
     github: "https://github.com/3AME/react-portfolio",
-    demo: "https://3ame.github.io/react-portfolio/",
-  }
+    demo: "https://anfang-react-portfolio.vercel.app/",
+  },
+  
 ]
 
 function Portfolio() {
@@ -84,7 +102,7 @@ function Portfolio() {
                   className="btn btn-primary"
                   target="_blank"
                 >
-                  Live Demo
+                  Live Demo/Website
                 </a>
               </div>
             </article>
